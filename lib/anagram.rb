f1f363bd383ef145a9afd511416a7cbf82f8abde
Iskra B <ibatistapoblete@gmail.com>
2019-05-22 04:26:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,13 +6,7 @@ class Anagram
     @word = word
   end
 
-  def match(%w(some words))
+  def match(words)
     some_word = @word.split("")
-    words.map do |x|
-
-    some_word.sort == words.sort
   end
-  end
-
-
 end
